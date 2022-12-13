1a5999927ae009b7110834835d5f1b6e5a8c12c1
Diego Garcia Cordeiro Souza <diego00alfa@gmail.com>
2022-12-12 21:54:36 -0300

    initial database controller
    Implements an initial database controller with GET and POST methods.
    Removed example MailService code.

--- src/mail/postmark-mail.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { MailService } from './mail.service';
-
-@Injectable()
-export class PostmarMailService implements MailService {
-  sendEmail(): string {
-    return 'Postmark Mail!';
-  }
-}
